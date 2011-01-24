a71bd5bd0b6499e9ae42f0e666a4c3d83821d3d0
Justin Love <git@JustinLove.name>
2011-01-23 21:45:09 -0600

    provide a way to access specific classes

--- spec/local_repository_spec.rb
+++ spec/local_repository_spec.rb
@@ -2,5 +2,6 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'amp_repo_spec/local_repository'
 
 describe Amp::Core::Repositories::Rugged::LocalRepository do
+  ModuleUnderTest = Amp::Core::Repositories::Rugged
   it_should_behave_like 'local repository'
 end
